c4b4812703a10eea0ba5549a72cf86820ae0793c
Nupur Negi <nupurn13@gmail.com>
2022-01-05 10:00:41 +0530

    add scikit

---
+++ build/temp.macosx-11-x86_64-3.9/sklearn/svm/src/libsvm/libsvm_template.o.d
@@ -0,0 +1,4 @@
+build/temp.macosx-11-x86_64-3.9/sklearn/svm/src/libsvm/libsvm_template.o: \
+  sklearn/svm/src/libsvm/libsvm_template.cpp \
+  sklearn/svm/src/libsvm/svm.cpp sklearn/svm/src/libsvm/svm.h
+commandline: -I/usr/local/lib/python3.9/site-packages/numpy/core/include -Ibuild/src.macosx-11-x86_64-3.9/numpy/distutils/include -c-msse -msse2 -msse3-I/usr/local/lib/python3.9/site-packages/numpy/core/include -Ibuild/src.macosx-11-x86_64-3.9/numpy/distutils/include
